d290a90299cfa841d525b56289319cd9b2786c37
Francis Scipik Tang <fltang@ucsc.edu>
2014-04-07 16:50:15 -0700

    Prototype
    Start of the prototype, if you add anything, make sure to put the associated metafile

---
+++ PongFire!/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+	void OnTriggerExit2D(Collider2D other) {
+		// Destroy anything that exits the boundary
+		Destroy (other.gameObject);
+	}
+}
---
+++ PongFire!/Assets/Scripts/ShotBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour {
+	// Variable to set rotation and shot speed
+	public float rotSpeed, speed;
+
+	// Use this for initialization
+	void Start () {
+		// Gives the shot a rotation
+		rigidbody2D.angularVelocity = rotSpeed;
+
+		// Gives forward velocity to shot wen fired
+		rigidbody2D.velocity = transform.right * speed;
+	}
+}
