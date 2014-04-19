fe29a0d30b4023f15de2846d828a347862d942c3
KeThieu <kthieu@ucsc.edu>
2014-04-18 22:16:26 -0700

    Added a proper Instruction Screen
    Needs a better job. Paint is quite limited...

--- PongFire!/Assets/Scripts/Menu/MainMenu.cs
+++ PongFire!/Assets/Scripts/Menu/MainMenu.cs
@@ -12,17 +12,7 @@ public class MainMenu : MonoBehaviour {
 	public Texture2D startImage;
 	public Texture2D howImage;
 	public Texture2D mainMenuImage;
-	
-	private string playInstructions = "PongFire! is a PvP game where the goal is for each players to shoot their puck into the opponent's goal using bullets. \n" +
-		"Each player uses the same keyboard. Whoever gets their puck into the opponent's goal first wins the game!\n\n" +
-			"Controls for Player 1 are: \n" +
-			"W to Move Up, S to Move Down\n" +
-			"D to fire bullets, A to Reload\n" +
-			"\n" +
-			"Controls for Player 2 are: \n" +
-			"I to Move Up, K to Move Down\n" +
-			"J to fire bullets, L to reload\n" +
-			"\n";
+	public Texture2D instructions;
 	
 	// Use this for initialization
 	void Start () {
@@ -41,7 +31,7 @@ public class MainMenu : MonoBehaviour {
 			}
 			break;
 		case MenuID.HowToPlay:
-			GUI.Label (new Rect(0,0, Screen.width, Screen.height / 3), playInstructions);
+			GUI.Label (new Rect(5,0, Screen.width, Screen.height), instructions);
 			if(GUI.Button (new Rect(Screen.width/2 - 100, Screen.height - 150, 200, 100), mainMenuImage)) {
 				currentMenu = MenuID.MainMenu;
 			}
