5eed7e67446d4ea22fdc3797b318a1f5f9ac3d41
Sarah Kohl <sarahcharlottekohl@gmail.com>
2014-04-26 19:48:32 -0700

    Song update, pucks no longer look like they're in molasses!  (I hope)

--- PongFire!/Assets/Scripts/Game/Puck.cs
+++ PongFire!/Assets/Scripts/Game/Puck.cs
@@ -8,6 +8,38 @@ public class Puck : MonoBehaviour {
 	
 	// Used to pause game when victory is met
 	public FreezeGame freezeScript;
+
+
+
+
+
+	void Update() {
+
+
+		//float randX = Random.Range (0,.0f);
+
+		transform.Translate(.01f, 0, 0,Space.Self); // move forward
+
+		if (redPuck) {
+						transform.Rotate (Vector3.back, Time.deltaTime * 60, Space.Self);
+				} else {
+						transform.Rotate (Vector3.forward, Time.deltaTime * 60, Space.Self);
+				}
+
+
+		//float randX = Random.Range (0,.05f);
+		//float randY = Random.Range (0,.05f);
+		//float randZ = Random.Range (0,.01f);
+
+		//transform.Translate(randX, randY, 0,Space.Self);
+
+		//transform.rotation = Quaternion.FromToRotation (Vector3.up, transform.forward);
+
+		//transform.Rotate(Vector3.forward,Time.deltaTime*60,Space.Self); // turn a little
+
+		//transform.Rotate (Vector3.forward, Time.deltaTime * 90, Space.World);
+	}
+
 	
 	// Performs action when Collision happens
 	void OnCollisionEnter2D(Collision2D collisionInfo) {
