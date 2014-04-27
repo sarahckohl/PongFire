04895c0a2c209e50648d5772b48215f5e2731d8c
Francis Scipik Tang <fltang@ucsc.edu>
2014-04-26 22:10:45 -0700

    Forcefield pushback
    Added a pushback force on forcefield

--- PongFire!/Assets/Scripts/Game/ForcefieldSound.cs
+++ PongFire!/Assets/Scripts/Game/ForcefieldSound.cs
@@ -5,14 +5,18 @@ public class ForcefieldSound : MonoBehaviour {
 	
 	public bool blueGoal;
 	
-	void OnCollisionEnter2D(Collision2D collisionInfo) {
+	public float pushBack;
+	
+	void OnTriggerEnter2D(Collider2D collisionInfo) {
 		if (blueGoal) {
 			if (collisionInfo.gameObject.name == "Blue Puck") {
 				audio.Play ();
+				collisionInfo.rigidbody2D.AddForce(new Vector2(pushBack, 0.0f));
 			}
 		} else {
 			if (collisionInfo.gameObject.name == "Red Puck") {
 				audio.Play ();
+				collisionInfo.rigidbody2D.AddForce(new Vector2(-pushBack, 0.0f));
 			}
 		}		
 	}
